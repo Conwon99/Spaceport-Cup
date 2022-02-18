5d76baed3358c0397a35c66cd329b180caae151f
Connor Dorward <70778006+Conwon99@users.noreply.github.com>
2022-02-18 22:15:13 +0000

    Update ESP32_LoRa_Flight_Computer.ino
    Added Multitasking - SD log still not working with GPS. Outputs lat=0 lon=0

--- Code+Software/Secondary Flight Computer Code/ESP32_LoRa_Flight_Computer.ino
+++ Code+Software/Secondary Flight Computer Code/ESP32_LoRa_Flight_Computer.ino
@@ -1,5 +1,3 @@
-
-
 /* ESP32 LoRa Flight Computer
  * Author : Connor Dorward
  * 
@@ -10,6 +8,10 @@
  * 
  */
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+/// BLUETOOTH////////
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -26,8 +28,52 @@ int charnum = 0;
 char incomingChar;
 
 
+#include <SoftwareSerial.h>
+#include <TinyGPS.h> 
 
 
+TinyGPS gps; // create gps object 
+float lat,lon; // create variable for latitude and longitude object  
+
+//HardwareSerial GPS_Serial(1);
+SoftwareSerial serialGPS(25,12);
+
+
+
+// SD CARD
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+
+#define SD_CS 13
+#define SD_CLK 14
+#define SD_MOSI 15
+#define SD_MISO 2
+
+bool SDfail=0;
+SPIClass sdSPI(HSPI);
+
+// Function prototypes
+
+
+
+
+float get_alt();
+float get_accel();
+void get_Readings();
+
+
+void setupSD(void);
+void writeFile(fs::FS &fs, const char,const char);
+void appendFile(fs::FS &fs, const char , const char);
+void log_SD(void);
+
+String BT_receive(int num_msgs);
+void BT_send_status();
+
+
+// BAROMETER & ACCELEROMETER////////
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
@@ -41,11 +87,134 @@ float altitude_sum, accel_sum;
 float alt_offset;
 
 
+
+
+void setup() {
+   Serial.begin(9600);
+
+   setupSD();
+
+   unsigned status;
+  status = bmp.begin();
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+
+  
+    /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+   
+   
+   serialGPS.begin(9600);
+
+   SerialBT.begin("ESP32"); //Bluetooth device name
+   Serial.println("The device started, now you can pair it with bluetooth!");
+
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+                    Task1code,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */                  
+  delay(500); 
+
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+                    Task2code,   /* Task function. */
+                    "Task2",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task2,      /* Task handle to keep track of created task */
+                    1);          /* pin task to core 1 */
+    delay(500); 
+
+  while(BT_receive(1) != "start"){}
+  get_Readings();
+
+    
+}
+
+//Task1code: blinks an LED every 1000 ms
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;)
+  {
+    Serial.print("Task1");
+
+    get_Readings();
+    //log_SD();
+    
+   // vTaskDelay(3333 / portTICK_PERIOD_MS);
+  } 
+}
+
+//Task2code: blinks an LED every 700 ms
+void Task2code( void * pvParameters )
+{
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;)
+  {
+   // Serial.print("Task2");
+   // vTaskDelay(1000 / portTICK_PERIOD_MS);
+   while (serialGPS.available() > 0) 
+      {
+       // Serial.print("h");
+  
+        if(gps.encode(serialGPS.read()))// encode gps data 
+        {  
+        gps.f_get_position(&lat,&lon); // get latitude and longitude 
+      
+        // display position 
+        Serial.print("Position: "); 
+        Serial.print("Latitude:"); 
+        Serial.print(lat,6); 
+        Serial.print(";"); 
+        Serial.print("Longitude:"); 
+        Serial.println(lon,6);  
+        } 
+
+      }
+  }
+}
+void loop() 
+
+{
+  
+  
+}
+
+
+
+
+
+
+
+
 float get_alt()
 {
-   Serial.print(F("Approx altitude = "));
-   Serial.print(bmp.readAltitude(1013.25)-alt_offset); /* Adjusted to local forecast! */
-   Serial.println(" m");
+   //Serial.print(F("Approx altitude = "));
+   //Serial.print(bmp.readAltitude(1013.25)-alt_offset); /* Adjusted to local forecast! */
+  // Serial.println(" m");
 
    return bmp.readAltitude(1013.25)-alt_offset;
    
@@ -112,6 +281,8 @@ void get_Readings()
 
 {
 
+  //get_coords();
+
   for (int i = 0; i <10;i++)  // Take 10 readings then average
   {
   
@@ -128,54 +299,100 @@ void get_Readings()
   altitude_sum =0;
   accel_sum=0;
 
+  Serial.print("Alt=");
+  Serial.println(avg_altitude);
+
+  Serial.print("Lat=");
+  Serial.println(lat);
+
+  Serial.print("Lon=");
+  Serial.println(lon);
+
+  Serial.println();
+
 }
 
-
-
-void setup() 
+void log_SD()
 {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-  delay(5000);
+// "Time , height, lat, lon , accel, state"
 
-  unsigned status;
-  status = bmp.begin();
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
-    while (1) delay(10);
-  }
 
-    /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+appendFile(SD, "/data.txt", "yowassuop");
 
-  while(BT_receive(1) != "start"){}
-  
-
-    
-  
-  //ADD a 'begin' BT message
-
-  get_Readings();
-  alt_offset= avg_altitude;
-  BT_send_status();  // Sends status data
-  Serial.print(avg_altitude);
-
-//SerialBT.write(avg_altitude)
 }
 
-void loop() {
+void setupSD() {
+  
+  Serial.println("Initializing SD card...");
+sdSPI.begin(SD_CLK, SD_MISO, SD_MOSI, SD_CS);
+if (!SD.begin(SD_CS, sdSPI)) {
 
-get_Readings();
+Serial.println("Card Mount Failed");
 
+SDfail=1;
+
+}
+
+else
+
+{
+Serial.println("Success");
+SDfail = 0;
+}
+uint8_t cardType = SD.cardType();
+if(cardType == CARD_NONE) {
+  Serial.println("No SD card attached");
+
+  SDfail=1;
+
+}
+
+File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "Vbat, Iin, Iout \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    SDfail = 1;
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+    SDfail = 1;
+  }
+  file.close();
+}
+
+
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    SDfail = 1;
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+    SDfail = 1;
+  }
+  file.close();
 }
